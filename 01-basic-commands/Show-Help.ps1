7164b36aa93471902fe7ec9d162411e1b2e1d756
Carter Pratt <Carter429@comcast.net>
2025-12-07 12:22:40 -0800

    Add Show-Help.ps1 script to demonstrate practice

---
+++ 01-basic-commands/Show-Help.ps1
@@ -0,0 +1,7 @@
+# Script: Show-Help.ps1
+# Purpose: Demonstrates how to access PowerShell
+# Author: Carter Pratt
+
+Write-Host "---Demonstrating PowerShell Help System ---"
+Get-Help Get-ChildItem
+Get-Help Get-Process
